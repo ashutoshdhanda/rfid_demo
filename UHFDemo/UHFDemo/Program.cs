fc6ecfa2f5b6e4b841e045eacac4d26a3de4ddbf
Ashutosh Dhanda <ashutoshdhanda@gmail.com>
2021-02-15 10:07:15 -0300

    this

---
+++ UHFDemo/UHFDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Data.SQLite;
+
+
+namespace UHFDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// Main
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //SQLiteConnection sqlite_conn;
+            //sqlite_conn = CreateConnection();
+            //CreateTable(sqlite_conn);
+            //InsertData(sqlite_conn);
+            //ReadData(sqlite_conn);
+            Application.Run(new R2000UartDemo());
+        }
+        static SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=" +
+                "database.db; Version = 3; New = True; Compress = True;");
+            sqlite_conn.Open();
+            // Open the connection:
+            return sqlite_conn;
+        }
+
+        static void CreateTable(SQLiteConnection conn)
+        {
+
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE SampleTable4" +
+                "(Col1 VARCHAR(20), Col2 INT)";
+           //string Createsql1 = "CREATE TABLE SampleTable4" +
+                //"(Col1 VARCHAR(20), Col2 INT)";
+           sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+            
+        }
+
+        static void InsertData(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable4" +
+                "(Col1, Col2) VALUES('Test Text ', 1); ";
+           sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable4" +
+                "(Col1, Col2) VALUES('Test1 Text1 ', 2); ";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable4" +
+                "(Col1, Col2) VALUES('Test2 Text2 ', 3); ";
+            sqlite_cmd.ExecuteNonQuery();
+
+
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable4" +
+                "(Col1, Col2) VALUES('Test3 Text3 ', 3); ";
+           sqlite_cmd.ExecuteNonQuery();
+
+        }
+
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM SampleTable4";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+            conn.Close();
+        }
+    }
+}
